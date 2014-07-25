fdfd0155e7207008de15f464e2ca70d5270e1d4f
Andreas Bernacca <andreas@agoodid.se>
2014-07-25 23:26:47 +0200

    1.6 urls

--- taggit/urls.py
+++ taggit/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('taggit.views',
     url(r'^list$', 'list_tags', name='taggit-list'),
 )
-
-
